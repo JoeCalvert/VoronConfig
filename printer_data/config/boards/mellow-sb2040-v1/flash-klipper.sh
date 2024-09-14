796b1aba59a50b9cac5470c3dffaa1210934c928
BackupBot <backup@voron>
2024-09-14 15:55:15 +0100

    flash-klipper.sh modified - 14/09/24 - 15:55:12

--- printer_data/config/boards/mellow-sb2040-v1/flash-klipper.sh
+++ printer_data/config/boards/mellow-sb2040-v1/flash-klipper.sh
@@ -4,12 +4,12 @@ cd "$HOME/klipper"
 
 
 flash(){
-	echo -e "\033[1;34m\nCleaning and building Klipper firmware for BTT MMB V1.1.\033[0m"
-	make clean KCONFIG_CONFIG=~/printer_data/config/boards/btt-mmb-v1_1/klipper_canbus.config
-	make -s KCONFIG_CONFIG=~/printer_data/config/boards/btt-mmb-v1_1/klipper_canbus.config -j4  > /dev/null 2>&1
-	mv ~/klipper/out/klipper.bin btt_mmb_v1_1.bin
-	echo -e "\033[1;34m\nFlashing Klipper to BTT MMB V1.1.\033[0m"
-	python3 ~/katapult/scripts/flash_can.py -f ~/klipper/btt_mmb_v1_1.bin -u c2d5f652b572
+	echo -e "\033[1;34m\nCleaning and building Klipper firmware for Mellow SB2040 V1.\033[0m"
+	make clean KCONFIG_CONFIG=~/printer_data/config/boards/mellow-sb2040-v1/klipper_canbus.config
+	make -s KCONFIG_CONFIG=~/printer_data/config/boards/mellow-sb2040-v1/klipper_canbus.config -j4  > /dev/null 2>&1
+	mv ~/klipper/out/klipper.bin mellow_sb2040_v1.bin
+	echo -e "\033[1;34m\nFlashing Klipper to Mellow SB2040 V1.\033[0m"
+	python3 ~/katapult/scripts/flash_can.py -f ~/klipper/mellow_sb2040_v1.bin -u 56993da7b11b
 }
 
 echo -e "\033[1;34m\nStopping Klipper service.\033[0m"
@@ -17,4 +17,5 @@ sudo service klipper stop
 sleep 5
 flash
 sleep 5
+echo -e "\033[1;34m\nStarting Klipper service.\033[0m"
 sudo service klipper start
