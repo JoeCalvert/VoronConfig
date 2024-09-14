712a89afb97245b3f0f5a60fb4efe15e9ff0d890
BackupBot <backup@voron>
2024-09-14 16:11:22 +0100

    flash-klipper.sh modified - 14/09/24 - 16:11:19

--- printer_data/config/boards/octopus-pro-f446/flash-klipper.sh
+++ printer_data/config/boards/octopus-pro-f446/flash-klipper.sh
@@ -4,12 +4,10 @@ cd "$HOME/klipper"
 
 
 flash(){
-	echo -e "\033[1;34m\nCleaning and building Klipper firmware for BTT MMB V1.1.\033[0m"
-	make clean KCONFIG_CONFIG=~/printer_data/config/boards/btt-mmb-v1_1/klipper_canbus.config
-	make -s KCONFIG_CONFIG=~/printer_data/config/boards/btt-mmb-v1_1/klipper_canbus.config -j4  > /dev/null 2>&1
-	mv ~/klipper/out/klipper.bin btt_mmb_v1_1.bin
-	echo -e "\033[1;34m\nFlashing Klipper to BTT MMB V1.1.\033[0m"
-	python3 ~/katapult/scripts/flash_can.py -f ~/klipper/btt_mmb_v1_1.bin -u c2d5f652b572
+	echo -e "\033[1;34m\nCleaning and building Klipper firmware for Octopus Pro F446\033[0m"
+	make clean KCONFIG_CONFIG=~/printer_data/config/boards/octopus-pro-f446/klipper_usb.config
+	echo -e "\033[1;34m\nFlashing Klipper to Octopus Pro F446.\033[0m"
+	make flash FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_stm32f446xx_570030001551303432323631-if00 KCONFIG_CONFIG=~/printer_data/config/boards/octopus-pro-f446/klipper_usb.config -j4  > /dev/null 2>&1
 }
 
 echo -e "\033[1;34m\nStopping Klipper service.\033[0m"
