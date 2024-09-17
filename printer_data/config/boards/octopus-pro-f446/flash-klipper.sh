c7b4673aee9b3c9420c536ca773ac42a21ab30dd
BackupBot <backup@voron>
2024-09-17 23:58:42 +0100

    New Backup on boot - 17/09/24 - 23:58:35

--- printer_data/config/boards/octopus-pro-f446/flash-klipper.sh
+++ printer_data/config/boards/octopus-pro-f446/flash-klipper.sh
@@ -3,17 +3,24 @@
 cd "$HOME/klipper"
 
 
-flash(){
+flash_usb(){
 	echo -e "\033[1;34m\nCleaning and building Klipper firmware for Octopus Pro F446\033[0m"
 	make clean KCONFIG_CONFIG=~/printer_data/config/boards/octopus-pro-f446/klipper_usb.config
 	echo -e "\033[1;34m\nFlashing Klipper to Octopus Pro F446.\033[0m"
 	make flash FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_stm32f446xx_570030001551303432323631-if00 KCONFIG_CONFIG=~/printer_data/config/boards/octopus-pro-f446/klipper_usb.config -j4  > /dev/null 2>&1
 }
 
+flash_uart(){
+        echo -e "\033[1;34m\nCleaning and building Klipper firmware for Octopus Pro F446\033[0m"
+        make clean KCONFIG_CONFIG=~/printer_data/config/boards/octopus-pro-f446/klipper_uart.config
+        echo -e "\033[1;34m\nFlashing Klipper to Octopus Pro F446.\033[0m"
+        make flash FLASH_DEVICE=/dev/ttyAMA0 KCONFIG_CONFIG=~/printer_data/config/boards/octopus-pro-f446/klipper_uart.config
+}
+
 echo -e "\033[1;34m\nStopping Klipper service.\033[0m"
 sudo service klipper stop
 sleep 5
-flash
+flash_usb
 sleep 5
 echo -e "\033[1;34m\nStarting Klipper service.\033[0m"
 sudo service klipper start
